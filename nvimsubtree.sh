76cf3d9351e66b88363deda5e0727861fe0fff9b
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2023-09-27 20:34:45 -0500

    switched from bespoke script to git-sub

--- nvimsubtree.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-prefix="--prefix .config/nvim https://github.com/Speedy6451/init.vim.git master"
-twoplus=`echo $@ | tr ' ' '\n' | tail -n +2 - | tr '\n' ' '`
-git subtree $1 $prefix $twoplus 
